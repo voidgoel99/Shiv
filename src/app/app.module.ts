e61fa51cef28f563cd90a286fd43e25d7042a044
voidgoel99 <goel.shivansh98@gmail.com>
2020-05-12 16:10:59 +0530

    Second Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,10 @@ import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 import { LoginComponent } from './login/login.component';
 import { MatDialogModule} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import {FormsModule} from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -45,7 +49,11 @@ import { MatDialogModule} from '@angular/material/dialog';
     MatCardModule,
     MatButtonModule,
     AppRoutingModule,
-    MatDialogModule
+    MatDialogModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule
   ],
   providers: [
     DishService,
